f17f5c9e4834dfe81108171905a685eca669e91b
William Wofford <27900656+Brungleby@users.noreply.github.com>
2022-02-19 10:56:43 -0600

    fixup

--- Scripts/Interaction/InteractorWatcher_UI.cs
+++ Scripts/Interaction/InteractorWatcher_UI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class InteractorWatcher_UI : InteractorFeedback
+public class InteractorWatcher_UI : InteractorWatcher
 {
     protected override void OnInteractibleChanged( Interactible other )
     {
